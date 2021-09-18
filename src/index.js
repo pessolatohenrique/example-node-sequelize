93cf31f1543cbf5609b54d3fe5e2443b4538b767
pessolatohenrique <pessolatohenrique@gmail.com>
2021-09-18 11:49:23 -0300

    Commit inicial

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const routes = require("./routes");
+// necessary to load strategies
+require("./auth/strategies");
+
+const app = express();
+routes(app);
+
+app.use((error, req, res, next) => {
+  console.log("error", error);
+  if (error.name === "SequelizeValidationError") {
+    res.status(400).json(error);
+    return next();
+  }
+
+  res.status(error.getStatusCode()).json({ message: error.message });
+  return next(error);
+});
+
+app.listen(3000, () => console.log("Server Started"));
+
+module.exports = app;
